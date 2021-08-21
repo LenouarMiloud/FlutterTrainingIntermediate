21dbf19652d9eb444fccf866adc3ed99ece5fa3d
LenouarMiloud <lenouarmiloud@gmail.com>
2021-08-21 19:27:25 +0200

    add popup menu

--- Widgets/slider_and_indicator_app/lib/main.dart
+++ Widgets/slider_and_indicator_app/lib/main.dart
@@ -12,11 +12,39 @@ class MyApp extends StatefulWidget{
   _State createState() => new _State();
 }
 
+enum countries{Algeria,Indonesia,Senghaphore,Malisia}
+
 class _State extends State<MyApp>{
 
   double _value = 0.0;
+  countries _selected = countries.Algeria;
+  String _question = 'what is your country of burn';
+  List<PopupMenuEntry<countries>> _items = [];
+
+  @override
+  void initState() {
+    for(countries country in countries.values) {
+      _items.add(new PopupMenuItem(
+        child: new Text(_getDisplay(country),),
+        value: country,
+      ));
+    }
+  }
 
   void _onChange(double value) => setState(()=> _value = value);
+
+  void _onSelecte(countries country){
+    setState((){
+      _selected = country;
+      _question = 'You Selected ${_getDisplay(country)}';
+    });
+  }
+
+  String _getDisplay(countries country){
+    int index = country.toString().indexOf('.');
+    index++;
+    return country.toString().substring(index);
+  }
 
   @override
   Widget build(BuildContext context){
@@ -43,7 +71,19 @@ class _State extends State<MyApp>{
                   value: _value,
                   valueColor: new AlwaysStoppedAnimation<Color>(Colors.amber),
                 ),
-              )
+              ),
+              new PopupMenuButton<countries>(
+                  child:  new Icon(Icons.input),
+                  initialValue: countries.Algeria,
+                  onSelected: _onSelecte,
+                  itemBuilder: (BuildContext context) {
+                    return _items;
+                  }
+              ),
+              new Container(
+                padding: new EdgeInsets.all(5.0),
+                child: new Text(_question),
+              ),
             ],
           ),
         ),
