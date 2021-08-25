a1a2b828e52db5bc4a116f308ef371373b4c67d3
LenouarMiloud <lenouarmiloud@gmail.com>
2021-08-25 18:47:47 +0200

    add group_chart and Pie chart

---
+++ Charts/group_charts/android/app/src/main/kotlin/com/fsociety/group_charts/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fsociety.group_charts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Charts/pie_chart_app/android/app/src/main/kotlin/com/fsociety/pie_chart_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fsociety.pie_chart_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
